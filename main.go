f903076cb4147a57d8870db48b8300b24dedc30a
Davide Salerno <davide.salerno@gmail.com>
2025-07-10 15:20:59 +0000

    feat: configurations for local development

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"time"
+
 	ctrl "github.com/davidesalerno/workforce-shift-scheduling/pkg/controller"
-	cors "github.com/gin-contrib/cors"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"POST", "GET", "PUT", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.AllowCredentials = true
+	config.MaxAge = 12 * time.Hour
 	router := gin.Default()
+	router.Use(cors.New(config))
 	router.POST("/schedule", ctrl.RestScheduleHandler)
-	router.Use(cors.Default())
 	router.Run(":8080")
 }
--- ui/workforce-shift-scheduling/src/api.js
+++ ui/workforce-shift-scheduling/src/api.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 const API_BASE = 'http://localhost:8080';
 
 export const getSchedule = async (payload) => {
-  const response = await axios.post(`${API_BASE}/schedule`, payload);
+  const apiUrl = import.meta.env.VITE_BASE_URL_OVERRIDE || API_BASE;
+  console.log(import.meta.env.VITE_BASE_URL_OVERRIDE) 
+  console.log(apiUrl); 
+  const response = await axios.post(`${apiUrl}/schedule`, payload);
   return response.data;
 };
